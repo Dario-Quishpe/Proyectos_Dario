d0e7335d04974a09b5b923c218c1aac2f98c35bf
Dario-Quishpe <dario.quishpe1999@hotmail.com>
2020-11-03 15:47:53 -0500

    clase 2 finalizada

---
+++ script#1.R
@@ -0,0 +1,19 @@
+#Ejemplo subir ficheros a git 
+
+# Parte#1 -----------------------------------------------------------------
+
+x=100
+y=200
+z=x+y
+print(z)
+if(x>y){
+  "es verdadero"
+}else{
+  "es falso"
+}
+help.search("lineal")
+rm(x)
+rm(list=ls())
+  x <- c(1:100)  
+length(x)
+x[1:20]  
